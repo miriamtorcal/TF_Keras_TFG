c9c7c8ce18d76da27a6cf62675600877bff72235
Miriam Torres <mtc1003@alu.ubu.es>
2022-07-07 21:04:27 +0200

    Pruebas flask

---
+++ pruebas previas/Flask/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+posts = []
+
+@app.route('/')
+
+def index():
+    return render_template("index.html", num_posts=len(posts))
+
+@app.route("/p/<string:slug>/")
+
+def show_post(slug):
+    return render_template("post_view.html", slug_title=slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
