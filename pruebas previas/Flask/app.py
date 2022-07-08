01a73da2574a9fbd435f4025fc55339e9327966c
Miriam Torres <mtc1003@alu.ubu.es>
2022-07-08 20:52:33 +0200

    Pruebas flask formularios

--- pruebas previas/Flask/app.py
+++ pruebas previas/Flask/app.py
@@ -1,7 +1,16 @@
+import secrets
 from flask import Flask
+from flask import request, redirect, url_for
 from flask import render_template
+from flask_wtf.csrf import CSRFProtect
+from forms import SignupForm, PostForm
+
 
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
+
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20ff'
 
 posts = []
 
@@ -15,8 +24,33 @@ def index():
 def show_post(slug):
     return render_template("post_view.html", slug_title=slug)
 
-@app.route("/admin/post/")
-@app.route("/admin/post/<int:post_id>/")
+@app.route("/admin/post/", methods=['GET', 'POST'], defaults={'post_id': None})
+@app.route("/admin/post/<int:post_id>/", methods=['GET', 'POST'])
 
-def post_form(post_id=None):
-    return render_template("admin/post_form.html", post_id=post_id)
+def post_form(post_id):
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        title_slug = form.title_slug.data
+        content = form.content.data
+
+        post = {'title': title, 'title_slug': title_slug, 'content': content}
+        posts.append(post)
+
+        return redirect(url_for('index'))
+    return render_template("admin/post_form.html", form=form)
+
+@app.route("/signup/", methods=["GET", "POST"])
+
+def show_signun_form():
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
+    return render_template("signup_form.html", form=form)
